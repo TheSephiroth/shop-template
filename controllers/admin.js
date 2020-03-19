a6e1b224d898724697b58b5a2bb2f357d7b99caa
TheSephiroth <igordani1996@gmail.com>
2020-03-19 16:49:56 -0100

    halfway through query params for editing

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,9 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
   });
 };
 
@@ -18,6 +15,13 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
 };
 
 exports.getProducts = (req, res, next) => {
