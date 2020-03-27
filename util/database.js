62bd2a8ecee26b47598c2e9f6f4dbb26749aaa92
TheSephiroth <igordani1996@gmail.com>
2020-03-27 16:41:13 -0100

    product Model Defined

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'uef20220721'
+const sequelize = new Sequelize('node-complete', 'root', 'uef20220721',{
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
