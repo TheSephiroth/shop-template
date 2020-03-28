d700143f49fc04e1b5d35ee79a050d975dd1cf16
TheSephiroth <igordani1996@gmail.com>
2020-03-28 19:54:43 -0100

    Delete Button Working

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
--- controllers/shop.js
+++ controllers/shop.js
@@ -105,6 +105,21 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
+  req.user
+    .getCart()
+    .then( cart => {
+      return cart.getProducts( {
+        where: { id: prodId}
+      })
+    })
+    .then(products => {
+      const product = products[0];
+     return product.cartItem.destroy();
+    })
+    .then( result => {
+       res.redirect('/cart')
+    })
+    .catch(err => {console.log(err)});
   Product.findByPk(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
