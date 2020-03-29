55889a66902aa050da8f0c0f20f3ced848172f69
TheSephiroth <igordani1996@gmail.com>
2020-03-29 19:06:07 -0100

    Reset card after order and Fetch Orders

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ Order.belongsToMany(Product, { through: OrderItem});
 
 // START
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -126,16 +126,46 @@ exports.postCartDeleteProduct = (req, res, next) => {
   });
 };
 
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+  .getCart()
+    .then( cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then( products => {
+      return req.user
+      .createOrder()
+        .then(order => {
+          return order.addProducts(
+            products.map(product => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+          }));
+        })
+        .catch( err => console.log(err))
+    })
+    .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch( err => console.log(err))
+  }
+
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user
+  .getOrders({include: ['products']})
+  .then( orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+  })
+  .catch(err => console.log(err))
+
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
